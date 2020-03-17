4a1a0622ac0fcb8bb8ae2b08b926bad50594215b
aswinpradeep <aswinpradeep1@gmail.com>
2020-03-17 23:55:52 +0530

    trial code

---
+++ DartCode.dart
@@ -0,0 +1,41 @@
+
+void main(){
+    TechLearnerMentor obj=TechLearnerMentor()
+    
+  
+}
+
+
+class TechLearnerMentor
+{
+    var stackname;
+    var participant;
+    var time;
+
+    void addStacks()
+    {
+      print("enter stackname");
+      this.stackname=stdin.readLineSync();
+
+    }
+    void setMentorOrLearner()
+    {
+      print("enter participant designation(type METOR OR LEARNER)");
+      this.participant= stdin.readLineSync();
+
+    }
+    void setAvailableTime()
+    {
+        if(this.participant=="MENTOR")
+        {
+          this.time=stdin.readLineSync();
+        }
+    }
+    void getMentor(var stackname,var time)
+    {
+      while(stackname)
+      {
+        if(this.)
+      }
+    }
+}
